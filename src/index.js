832a18fae5270574951c2ee12dd5ed70dcc00640
awadh <awadh.hassan@student.moringaschool.com>
2022-08-23 22:05:32 +0300

    assignmement done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  console.log("after DOM loaded ")
+  
+})
+
+console.log("before DOM loads")
+console.log (document.querySelector("div"))
+
+
+
